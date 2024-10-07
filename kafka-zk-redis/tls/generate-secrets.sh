ac51ece7be5cdf86cd1bd5f4d58e0c8e24a9c5ed
matharm <matharm@kali.local>
2024-10-07 14:25:42 +0300

    myol: issue of ssh and https

--- kafka-zk-redis/tls/generate-secrets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-secret_name=$1
-namespace=$2
-file_name=$3
-
-kubectl create secret generic ${secret_name}-jks -n $namespace --from-file=${file_name}.truststore.jks=./${secret_name}.truststore.jks --from-file=${file_name}.keystore.jks=./${secret_name}.keystore.jks
